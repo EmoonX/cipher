464c6e42bc88fff0ee4c203cb35e8914fbd08ea6
Gustavo Chicato <gustavo.wandeur@usp.br>
2019-10-17 20:12:51 -0300

    Flashing interactive door

--- Interactive.gd
+++ Interactive.gd
@@ -1,9 +1,22 @@
 extends Spatial
 
 var active = false
+var cont = 0
 
 func _on_Area_area_entered(area):
 	active = true
 
 func _on_Area_area_exited(area):
 	active = false
+
+func _process(delta):
+	if active:
+		var value = 1.0 - cont/120.0
+		if value < 0.5:
+			value = 1.0 - value
+		$MeshInstance.material_override.albedo_color.r = value
+		$MeshInstance.material_override.albedo_color.b = value
+		cont = (cont + 1) % 120
+	else:
+		cont = 0
+		$MeshInstance.material_override.albedo_color = Color(1, 1, 1)
