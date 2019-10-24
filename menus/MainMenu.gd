5e286adac7bdfdce7552acd97cb30613a8569603
Gustavo Chicato <gustavo.wandeur@usp.br>
2019-10-23 21:16:32 -0300

    More substancial main menu

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -1,4 +1,14 @@
 extends Control
-
-func _ready():
-	$Start.grab_focus()
+	
+func _process(delta):
+	if $Fade.color.a > 0.0:
+		var value = $Fade.color.a - delta/2
+		$Fade.color.a = max(value, 0.0)
+		if $Fade.color.a == 0.0:
+			$Fade.visible = false
+			$Start.grab_focus()
+	
+	if $Start.pressed:
+		get_tree().change_scene("res://Game.tscn")
+	elif $Quit.pressed:
+		get_tree().quit()
