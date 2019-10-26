0fdfa11bdb2c47b9867987b5e3e44697c83d9e81
Gustavo Chicato <gustavo.wandeur@usp.br>
2019-10-25 21:49:31 -0300

    Enable changing OptionButtons content with <- and -> keys

--- menus/Options.gd
+++ menus/Options.gd
@@ -2,9 +2,20 @@ extends Control
 
 # --------------------------------------------------------------------------- #
 
-func _ready():
-	var languages = ["English", "Português (Brasil)"]
-	for id in range(len(languages)):
-		$Language/OptionButton.add_item(languages[id], id)
-	
+func _ready():	
 	$Language/OptionButton.grab_focus()
+
+func _process(delta):
+	# Enable changing OptionButtons content with <- and -> keys
+	for node in [$Language, $Resolution]:
+		var button = node.get_node("OptionButton")
+		if button.has_focus():
+			if Input.is_action_just_pressed("ui_left"):
+				if button.selected > 0:
+					button.selected -= 1
+			elif Input.is_action_just_pressed("ui_right"):
+				if button.selected < button.get_item_count() - 1:
+					button.selected += 1
+				
+	if $Back.pressed:
+		get_tree().change_scene("res://menus/MainMenu.tscn")
