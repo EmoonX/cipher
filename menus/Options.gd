1c796dfefa299f3340958e10e3edc9cadfc18e3f
Gustavo Chicato <gustavo.wandeur@usp.br>
2019-10-25 23:02:21 -0300

    Change between screen modes (windowed, fullscreen, etc)

--- menus/Options.gd
+++ menus/Options.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var mode = $ScreenMode/OptionButton
+
 # --------------------------------------------------------------------------- #
 
 func _ready():	
@@ -7,7 +9,7 @@ func _ready():
 
 func _process(delta):
 	# Enable changing OptionButtons content with <- and -> keys
-	for node in [$Language, $Resolution]:
+	for node in [$Language, $ScreenMode]:
 		var button = node.get_node("OptionButton")
 		if button.has_focus():
 			if Input.is_action_just_pressed("ui_left"):
@@ -16,6 +18,16 @@ func _process(delta):
 			elif Input.is_action_just_pressed("ui_right"):
 				if button.selected < button.get_item_count() - 1:
 					button.selected += 1
-				
+	
+	match mode.get_item_text(mode.selected):
+		"Window":
+			OS.window_fullscreen = false
+			OS.window_borderless = false
+		"Borderless Window":
+			OS.window_fullscreen = false
+			OS.window_borderless = true
+		"Borderless Fullscreen":
+			OS.window_fullscreen = true
+	
 	if $Back.pressed:
 		get_tree().change_scene("res://menus/MainMenu.tscn")
