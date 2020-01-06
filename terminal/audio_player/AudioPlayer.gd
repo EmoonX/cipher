2a85caeb3de02190581e02be7c6c45a7c6fc0af8
Gustavo Chicato <gustavo.chicato@usp.br>
2020-01-05 21:19:05 -0300

    AudioPlayer: slider seeking and positioning working nicely

--- terminal/audio_player/AudioPlayer.gd
+++ terminal/audio_player/AudioPlayer.gd
@@ -1,13 +1,23 @@
 extends "res://terminal/Program.gd"
+
 # --------------------------------------------------------------------------- #
 
 func _process(delta):
+	# Set slider length as audio length
+	$GUI/Main/Slider.max_value = $Player.stream.get_length()
+	
+	# Slider goes together with the music ~
+	if $Player.playing and not $Player.stream_paused:
+		$GUI/Main/Slider.value = $Player.get_playback_position()
+	
 	# Check button presses
 	if $GUI/Main/Buttons/Play.pressed:
-		if not $Player.playing:
+		if not $Player.playing or $Player.stream_paused:
 			$Player.playing = true
+			$Player.seek($GUI/Main/Slider.value)
 		$Player.stream_paused = false
 	elif $GUI/Main/Buttons/Pause.pressed:
 		$Player.stream_paused = true
 	elif $GUI/Main/Buttons/Stop.pressed:
 		$Player.playing = false
+		$GUI/Main/Slider.value = 0.0
