477747bb161f95fdbf347c23ab286b5a3724a42d
Gustavo Chicato <gustavo.chicato@usp.br>
2020-01-07 08:13:23 -0300

    Sudoku: little highlight to help putting numbers on grid

--- puzzles/sudoku/Number.gd
+++ puzzles/sudoku/Number.gd
@@ -1,5 +1,7 @@
 extends Label
 
+const box = preload("res://puzzles/sudoku/Box.tres")
+
 # Position on the grid
 var i
 var j
@@ -11,13 +13,23 @@ func _ready():
 
 func _process(delta):
 	if has_focus():
+		set("custom_styles/normal", box)
+		
 		for number in range(KEY_1, KEY_9 + 1):
+			# Ckeck number presses
 			if Input.is_key_pressed(number):
 				text = str(number - KEY_0)
 				$"../..".grid[i][j] = text
+				
+				# Color it acordingly
 				var color = ColorN("green" if $"../..".check(i, j) else "red")
 				set("custom_colors/font_color", color)
+			
+			# Check also for delete/backspace presses
 			elif Input.is_key_pressed(KEY_DELETE) or \
 					Input.is_key_pressed(KEY_BACKSPACE):
 				text = " "
 				$"../..".grid[i][j] = text
+
+	else:
+		set("custom_styles/normal", StyleBoxEmpty.new())
