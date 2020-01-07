935eea57e49bd137114c1f2cd8f4090fff63c5bf
Gustavo Chicato <gustavo.chicato@usp.br>
2020-01-06 21:11:57 -0300

    Chess puzzle: solution working as intended

--- puzzles/chess/Chess.gd
+++ puzzles/chess/Chess.gd
@@ -1,14 +1,36 @@
 extends Node
 
-var aux = "abcdefgh"
+var ranks = "abcdefgh"
+var positions = []
 
 # --------------------------------------------------------------------------- #
 
 func _ready():
+	# Load piece sprites and position them accordingly
 	for board in [$BoardA, $BoardB]:
 		for piece in board.get_children():
 			var sprite = piece.type + "_" + piece.color + ".png"
 			piece.get_node("Sprite").texture = \
 					load("res://puzzles/chess/" + sprite)
-			piece.rect_position.x = -448 + 128 * aux.find(piece.position[0])
-			piece.rect_position.y = +576 - 128 * int(piece.position[1])
+			piece.rect_position.x = -520 + 128 * ranks.find(piece.position[0])
+			piece.rect_position.y = +504 - 128 * int(piece.position[1])
+	
+	# Save which positions appear simultaneously on both boards
+	for a in $BoardA.get_children():
+		for b in $BoardB.get_children():
+			if a.position == b.position:
+				positions.append(a.position)
+				break
+
+func _process(delta):
+	# Check if there are any wrongly active/inactive pieces
+	print(positions)
+	for board in [$BoardA, $BoardB]:
+		for piece in board.get_children():
+			if piece.modulate.a == 1.0 and not piece.position in positions:
+				return
+			if piece.modulate.a != 1.0 and piece.position in positions:
+				return
+	
+	# If piece positions on boards A and B are all equal, you win!
+	get_tree().quit()
--- puzzles/chess/Piece.gd
+++ puzzles/chess/Piece.gd
@@ -3,3 +3,15 @@ extends Control
 export(String) var color
 export(String) var type
 export(String) var position
+
+# --------------------------------------------------------------------------- #
+
+func _on_Piece_focus_entered():
+	# Turn or unturn piece transparent when clicked
+	if modulate.a == 1.0:
+		modulate = Color(1, 1, 1, 0.1)
+	else:
+		modulate = Color(1, 1, 1, 1.0)
+	
+	# Make piece clickable again immediately
+	release_focus()
