b1f6360f063651a5f90bd4404cf98c43abe68d4e
Gustavo Chicato <gustavo.chicato@usp.br>
2020-01-08 19:44:08 -0300

    Lights Out: game is playable

--- puzzles/lights_out/Button.gd
+++ puzzles/lights_out/Button.gd
@@ -7,12 +7,4 @@ var j
 # --------------------------------------------------------------------------- #
 
 func _on_Button_toggled(button_pressed):
-	var positions = [
-		Vector2(i-1, j), Vector2(i+1, j), Vector2(i, j-1), Vector2(i, j+1)
-	]
-	for pos in positions:
-		if pos.x < 0 or pos.x >= $"../..".height or \
-				pos.y < 0 or pos.y >= $"../..".width:
-			continue
-		var button = $"../..".grid[pos.x][pos.y]
-		button.pressed = not button.pressed
+	$"../..".toggle(i, j)
--- puzzles/lights_out/LightsOut.gd
+++ puzzles/lights_out/LightsOut.gd
@@ -6,7 +6,10 @@ const Button = preload("res://puzzles/lights_out/Button.tscn")
 export(int) var height = 5
 export(int) var width = 5
 
-# Boolean atrix representing grid state
+# How many random clicks are done before playing
+export(int) var clicks = 5
+
+# Boolean matrix representing grid state
 var grid = []
 
 # --------------------------------------------------------------------------- #
@@ -20,6 +23,35 @@ func _ready():
 			var button = Button.instance()
 			button.i = i
 			button.j = j
-			button.pressed = false
+			button.pressed = true
 			$Grid.add_child(button)
 			grid[i].append(button)
+	
+	# Click randomly to generate initial grid
+	for x in range(clicks):
+		var i = randi() % height
+		var j = randi() % width
+		toggle(i, j)
+
+func toggle(i, j):
+	# Toggle neighbour positions of clicked button
+	var positions = [
+		Vector2(i-1, j), Vector2(i+1, j), Vector2(i, j-1), Vector2(i, j+1)
+	]
+	for pos in positions:
+		if pos.x < 0 or pos.x >= height or pos.y < 0 or pos.y >= width:
+			continue
+		var button = grid[pos.x][pos.y]
+		button.pressed = not button.pressed
+
+func _solved():
+	# Check if all buttons are lightened up
+	for i in range(height):
+		for j in range(width):
+			if not grid[i][j].pressed:
+				return false
+	return true
+
+func _process(delta):
+	if _solved():
+		get_tree().quit()
