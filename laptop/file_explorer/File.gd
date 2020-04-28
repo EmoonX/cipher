dfd669e44a62b8cfb12917c93d72489ed01576ce
Gustavo Chicato <gustavo.chicato@usp.br>
2020-04-28 01:29:02 -0300

    [File Explorer] Inner folder navigation

--- laptop/file_explorer/File.gd
+++ laptop/file_explorer/File.gd
@@ -7,3 +7,9 @@ func _on_File_focus_entered():
 
 func _on_File_focus_exited():
 	get_node("Name").set("custom_colors/font_color", ColorN("black"))
+
+func _on_File_gui_input(event: InputEvent):
+	if event.is_action_pressed("ui_accept") or \
+			event is InputEventMouseButton and event.doubleclick:
+		$"../..".path += $Name.text + "/"
+		$"../..".show_files()
--- laptop/file_explorer/FileExplorer.gd
+++ laptop/file_explorer/FileExplorer.gd
@@ -2,25 +2,39 @@ extends ColorRect
 
 const _File = preload("File.tscn")
 
-onready var path = "res://user/files/"
+var cwd = Directory.new()
+var path = "res://user/files/"
 
 # --------------------------------------------------------------------------- #
 
 func _ready():
-	_show_files()
+	show_files()
 	
-func _show_files():
+func show_files():
+	# Clean up previous directory files (if any)
+	for file in $FileGrid.get_children():
+		file.queue_free()
+	
 	# Show current directory files (including folders)
-	var cwd = Directory.new()
+	var files = []
 	cwd.change_dir(path)
 	cwd.list_dir_begin(true)
 	while true:
 		var filename = cwd.get_next()
 		if not filename:
 			break
-		if cwd.current_is_dir():
-			var file = _File.instance()
-			file.get_node("Icon").texture = \
-					load("res://laptop/file_explorer/icons/folder.png")
-			file.get_node("Name").text = filename
-			$FileGrid.add_child(file)
+		if ".import" in filename:
+			continue
+		files.append(filename)
+	files.sort()
+	for filename in files:
+		var file = _File.instance()
+		
+		var icon
+		if not "." in filename:
+			# If it doesn't have an extension, it must be a folder
+			icon = load("res://laptop/file_explorer/icons/folder.png")
+			
+		file.get_node("Icon").texture = icon
+		file.get_node("Name").text = filename
+		$FileGrid.add_child(file)
