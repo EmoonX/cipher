b4efbfad2622b21003e9de1cab87018866195d22
Gustavo Chicato <gustavo.chicato@usp.br>
2020-04-28 23:09:34 -0300

    [Map] Centers and moves accordingly to player movement

--- laptop/map/Map.gd
+++ laptop/map/Map.gd
@@ -2,19 +2,29 @@ extends Control
 
 const MapRoom = preload("res://laptop/map/Room.tscn")
 
+# The scaling factor
+const SCALE = 8.0
+
 # --------------------------------------------------------------------------- #
 
 func register_room(room, door=null):
 	# Get top-down representation of the room by its floor dimensions
-	var size = 8 * room.get_node("Box/Floor_plane").mesh.size
+	var size = SCALE * room.get_node("Box/Floor_plane").mesh.size
 	var scale = room.get_node("Box").scale
 	size.x *= scale.x
 	size.y *= scale.z
 	
 	# Add correctly scaled room to the map
 	var map_room = MapRoom.instance()
-	add_child(map_room)
+	$Rooms.add_child(map_room)
 	map_room.rect_size = size
+	map_room.rect_position = Vector2(0, 0) - size/2
+
+func _position_player():
+	# Update player position by making whole map re-center on it
+	var player = $"/root/Game/Player"
+	var pos = SCALE * Vector2(player.translation.x, player.translation.z)
+	$Rooms.rect_position = get_viewport_rect().size/2 + pos
 
 func _process(delta):
 	# Enter or exit map
@@ -22,9 +32,11 @@ func _process(delta):
 			not get_tree().paused:
 		visible = true
 		$"..".visible = true
-		$"/root/Game".pause_toggle()
+#		$"/root/Game".pause_toggle()
 	elif visible and Input.is_action_just_pressed("map"):
 		visible = false
 		$"..".visible = false
-		$"/root/Game".pause_toggle()
+#		$"/root/Game".pause_toggle()
 	Input.action_release("map")
+	
+	_position_player()
