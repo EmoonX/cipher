aa7fdae9e6da355f80796fc98c7e2a53a1409a0a
Gustavo Chicato <gustavo.chicato@usp.br>
2020-04-29 01:17:17 -0300

    [Map] Rooms doors scanned and added automatically

--- laptop/map/Map.gd
+++ laptop/map/Map.gd
@@ -1,13 +1,14 @@
 extends Control
 
 const MapRoom = preload("res://laptop/map/Room.tscn")
+const MapDoor = preload("res://laptop/map/Door.tscn")
 
 # The scaling factor
 const SCALE = 8.0
 
 # --------------------------------------------------------------------------- #
 
-func register_room(room, door=null):
+func register_room(room, entry_door=null):
 	# Get top-down representation of the room by its floor dimensions
 	var size = SCALE * room.get_node("Box/Floor_plane").mesh.size
 	var scale = room.get_node("Box").scale
@@ -19,6 +20,17 @@ func register_room(room, door=null):
 	$Rooms.add_child(map_room)
 	map_room.rect_size = size
 	map_room.rect_position = Vector2(0, 0) - size/2
+	
+	# Add doors to map
+	for node in room.get_children():
+		if not "Door" in node.name:
+			continue
+		var door = node
+		var pos = SCALE * Vector2(door.translation.x, door.translation.z)
+		var map_door = MapDoor.instance()
+		$Rooms.add_child(map_door)
+		map_door.rect_position = -pos - (map_door.rect_size / 2)
+		map_door.rect_rotation = door.rotation_degrees.y
 
 func _position_player():
 	# Update player position by making whole map re-center on it
@@ -29,7 +41,6 @@ func _position_player():
 	# Rotate player accordingly
 	var rot = 180.0 - player.rotation_degrees.y
 	$Player.rect_rotation = rot
-	print(rot)
 
 func _process(delta):
 	# Enter or exit map
