8c8678da686114646b9d11e171a47435a7a6cd25
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-03 10:14:07 -0300

    [3D] Glass window and environment lighting

--- objects/LightSwitch.gd
+++ objects/LightSwitch.gd
@@ -1,11 +1,16 @@
 extends "res://objects/Interactable.gd"
 
+# If lights are turned on
+var turned_on = false
+
 # --------------------------------------------------------------------------- #
 
 func _process(delta):
 	$ActionLabel.text = \
-			"ACTION_TURNOFF" if $"../Lamps".visible else "ACTION_TURNON"
+			"ACTION_TURNON" if not turned_on else "ACTION_TURNON"
+	
 	if active and Input.is_action_just_pressed("action"):
+		turned_on = not turned_on
 		for node in $"../Lamps".get_children():
 			if node is OmniLight:
 				node.visible = not node.visible
