2104dac35d524edaf83f70157b87d8f53e971eb7
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-08 19:03:17 -0300

    [Interface] Added remaining action PNG icons

--- menus/actions/ActionInterface.gd
+++ menus/actions/ActionInterface.gd
@@ -1,4 +1,12 @@
 extends VBoxContainer
 
+# Initial position x of container
+onready var x_ini = rect_position.x
+
 # --------------------------------------------------------------------------- #
 
+func _process(delta):
+	# Position node correctly on screen
+	var offset = OS.window_size.x - OS.window_size.y * 16.0/9.0
+	offset = max(0, offset)
+	rect_position.x = x_ini + (offset * (1080 / OS.window_size.y) / 2)
