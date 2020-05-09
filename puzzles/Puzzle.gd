9a6c6eb4ce0303f43d49dd70c758dbd13730b2d4
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-09 20:04:35 -0300

    [Puzzles] Make smooth "delayed" interpolation to camera movement

--- puzzles/Puzzle.gd
+++ puzzles/Puzzle.gd
@@ -6,6 +6,7 @@ onready var initial_pos = $Camera.translation
 
 func _ready():
 	set_process(false)
+	set_process_input(false)
 
 func _on_Puzzle_visibility_changed():
 	if visible:
@@ -15,10 +16,21 @@ func _on_Puzzle_visibility_changed():
 		$"/root/Game".active_object = null
 		
 	set_process(visible)
+	set_process_input(visible)
 
-func _process(_delta):
+func _input(event):
 	# Make camera lightly follow mouse movement on screen
-	var mouse_pos = get_viewport().get_mouse_position()
-	var mouse_offset = (mouse_pos - (OS.window_size / 2))
-	$Camera.translation.x = initial_pos.x + (mouse_offset.x / 20000)
-	$Camera.translation.y = initial_pos.y - (mouse_offset.y / 10000)
+	if event is InputEventMouseMotion:
+		# Calculate mouse offset based on its position relative to center
+		var mouse_pos = get_viewport().get_mouse_position()
+		var mouse_offset = mouse_pos - (OS.window_size / 2)
+		
+		# Calculate new adjusted camera translation
+		var x_new = initial_pos.x + (mouse_offset.x / 20000)
+		var y_new = initial_pos.y - (mouse_offset.y / 10000)
+		var pos_new = Vector3(x_new, y_new, $Camera.translation.z)
+		
+		# Make smooth "delayed" interpolation to next position
+		$Camera/Tween.interpolate_property($Camera, "translation", \
+				null, pos_new, 1.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		$Camera/Tween.start()
