7c60e1d244d36c35f5e303981aec74e7a412d5e7
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-10 03:48:29 -0300

    [3D / Puzzles] Correct object dragging with collision avoidance

--- puzzles/Draggable.gd
+++ puzzles/Draggable.gd
@@ -1,5 +1,8 @@
 extends "res://objects/Interactable.gd"
 
+# Previously registered mouse position on screen
+var prev_mouse_pos
+
 # --------------------------------------------------------------------------- #
 
 func _ready():
@@ -7,20 +10,35 @@ func _ready():
 	_stop_drag()
 
 func _start_drag():
-	set_process_input(true)
 	set_physics_process(true)
+	prev_mouse_pos = get_viewport().get_mouse_position()
 
 func _stop_drag():
-	set_process_input(false)
 	set_physics_process(false)
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		pass
-		
-	elif event is InputEventMouseButton:
-		_stop_drag()
 		
 func _physics_process(delta):
-	$KinematicBody.move_and_slide(Vector3(1, 0, 0))
+	# Calculate offset based on mouse position difference
+	var mouse_pos = get_viewport().get_mouse_position()
+	var mouse_offset = mouse_pos - prev_mouse_pos
+	prev_mouse_pos = mouse_pos
+
+	# Calculate real offset on surface
+	var real_offset = \
+			Vector3(mouse_offset.x / 2000, 0.0, mouse_offset.y / 1500)
+	
+	# Move (or try to) kinmeatic body
+	var prev_pos = $KinematicBody.translation
+	$KinematicBody.translation += real_offset
+	var collision = $KinematicBody.move_and_collide(Vector3(0, 0, 0))
+	if collision:
+		# In case of collision detected, check if is indeed horizontal
+		var normal = collision.normal
+		print(normal)
+		if abs(normal.y) < 0.9:
+			$KinematicBody.translation = prev_pos
+			return
+	
+	# If everything's okay, Move meshes together
 	$Meshes.transform = $KinematicBody.transform
+	
+	
