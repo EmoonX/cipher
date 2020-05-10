15bbe6645ef7f27491d9af6bccbac7a29415a1f5
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-10 00:25:14 -0300

    [3D / Puzzles] Correct mouse highlighting of puzzle objects

--- puzzles/Puzzle.gd
+++ puzzles/Puzzle.gd
@@ -2,6 +2,9 @@ extends Spatial
 
 onready var initial_pos = $Camera.translation
 
+# Position of the mouse on the viewport
+var mouse_pos = Vector2(0, 0)
+
 # --------------------------------------------------------------------------- #
 
 func _ready():
@@ -17,12 +20,14 @@ func _on_Puzzle_visibility_changed():
 		
 	set_process(visible)
 	set_process_input(visible)
+	
+	$"/root/Game/Player".visible = not visible
 
 func _input(event):
 	# Make camera lightly follow mouse movement on screen
 	if event is InputEventMouseMotion:
 		# Calculate mouse offset based on its position relative to center
-		var mouse_pos = get_viewport().get_mouse_position()
+		mouse_pos = get_viewport().get_mouse_position()
 		var mouse_offset = mouse_pos - (OS.window_size / 2)
 		
 		# Calculate new adjusted camera translation
@@ -34,3 +39,15 @@ func _input(event):
 		$Camera/Tween.interpolate_property($Camera, "translation", \
 				null, pos_new, 1.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		$Camera/Tween.start()
+
+func _process(_delta):
+	# Cast ActionRay correctly to mouse position
+	var action_ray = $Camera/ActionRay
+	var to = $Camera.project_local_ray_normal(mouse_pos)
+	action_ray.cast_to = to * 100
+	
+	# Detect highlighted object by ray collision
+	var object = action_ray.get_collider()
+	if object:
+		object = object.get_parent()
+	$"/root/Game".active_object = object
