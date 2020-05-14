c21f90906d2377972d99fd03d434166f7b973f06
Gustavo Chicato <gustavo.chicato@usp.br>
2020-05-14 12:31:19 -0300

    [Puzzles] Snapping procedures seemingly working

--- puzzles/Draggable.gd
+++ puzzles/Draggable.gd
@@ -1,6 +1,9 @@
 extends "res://objects/Interactable.gd"
 class_name Draggable
 
+# Virtual object translation (for snapping)
+onready var virtual_pos = translation
+
 # Previously registered mouse position on screen
 var prev_mouse_pos
 
@@ -22,30 +25,29 @@ func _physics_process(_delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	var mouse_offset = mouse_pos - prev_mouse_pos
 	prev_mouse_pos = mouse_pos
-	
-	if mouse_offset == Vector2(0, 0):
-		puzzle.snap_to_led()
-		return
 
 	# Calculate real offset based on camera view
 	var real_offset = \
 			Vector3(mouse_offset.x / 3000, 0.0, mouse_offset.y / 1500)
 	
 	# Move (or try to) kinematic body
-	var prev_pos = $KinematicBody.translation
-	var new_pos = prev_pos + real_offset
-	$KinematicBody.translation = new_pos
+	var virtual_offset = virtual_pos - translation
+	$KinematicBody.translation = real_offset + virtual_offset
 	var collision = $KinematicBody.move_and_collide(Vector3(0, 0, 0))
 	if collision:
 		# In case of collision detected, check if is indeed horizontal
 		var normal = collision.normal
 		if abs(normal.y) < 0.9:
-			$KinematicBody.translation = prev_pos
+			$KinematicBody.translation = Vector3(0, 0, 0)
 			return
 	
 	# If everything's okay, move object together
-	translation += $KinematicBody.translation
+	virtual_pos += ($KinematicBody.translation - virtual_offset)
 	$KinematicBody.translation = Vector3(0, 0, 0)
+	var snap = puzzle.check_and_snap(self)
+	if not snap:
+		translation = virtual_pos
+	print(virtual_pos)
 
 func _process(_delta):
 	if Input.is_action_just_released("left_click"):
--- puzzles/nightstand/Nightstand.gd
+++ puzzles/nightstand/Nightstand.gd
@@ -1,22 +1,24 @@
 extends "res://puzzles/Puzzle.gd"
 
-const DIST_EPS = 0.05
+const DIST_EPS = 0.02
 
 onready var pos_lamp = $"../BedsideLamp".translation
 onready var pos_cactus = $"../Cactus".translation
 
 # -------------------------------------------------------------------------- #
 
+func check_and_snap(object: Draggable):
+	# Check if object is in position to be snapped
+	# If positive, translate it to snap position
+	for pos in [pos_lamp, pos_cactus]:
+		var dist = (object.virtual_pos - pos).length()
+		if dist < DIST_EPS:
+			object.translation = pos
+			return true
+	return false
+
 func _is_solved():
-	# Puzzle is solved if objects are in swaped positions
+	# Return if objects are in swaped positions
 	var lamp_ok = ($"../BedsideLamp".translation == pos_cactus)
 	var cactus_ok = ($"../Cactus".translation == pos_lamp)
-	return lamp_ok and cactus_ok
-
-func snap_to_led():
-	for object in [$"../BedsideLamp", $"../Cactus"]:
-		for pos in [pos_lamp, pos_cactus]:
-			var dist = (object.translation - pos).length()
-			if dist < DIST_EPS:
-				object.translation = pos
-				break
+	return (lamp_ok and cactus_ok)
