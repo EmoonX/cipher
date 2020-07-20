c7c3137e2578d37f17b11d8d5fdf44de4a977d76
Gustavo Chicato <gustavo.chicato@usp.br>
2020-07-20 02:10:34 -0300

    Glasses positioned correctly

--- puzzles/wine_glasses/WineGlasses.gd
+++ puzzles/wine_glasses/WineGlasses.gd
@@ -9,7 +9,9 @@ func _ready():
 	for row in $Rows.get_children():
 		var i = int(row.name) - 1
 		var cols = pow(2, i)
+		var x0 = -50 + ((1920 - 1400) / 2) + (1400 / (2 * cols))
+		var dx = 1400 / cols
 		for j in cols:
 			var glass = GlassEmpty.instance()
-			glass.rect_position.x += 200 * j
+			glass.rect_position.x = x0 + (j * dx)
 			row.add_child(glass)
