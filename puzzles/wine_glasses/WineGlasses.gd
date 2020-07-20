5504b4efcddb81b4004e4266aea8584633cb6852
Gustavo Chicato <gustavo.chicato@usp.br>
2020-07-20 02:20:54 -0300

    Glasses randomized between empty and half-full

--- puzzles/wine_glasses/WineGlasses.gd
+++ puzzles/wine_glasses/WineGlasses.gd
@@ -11,7 +11,16 @@ func _ready():
 		var cols = pow(2, i)
 		var x0 = -50 + ((1920 - 1400) / 2) + (1400 / (2 * cols))
 		var dx = 1400 / cols
+		var k
 		for j in cols:
-			var glass = GlassEmpty.instance()
+			if j % 2 == 0:
+				k = randi() % 2
+			else:
+				k = 1 if k == 0 else 0
+			var glass
+			if k == 0:
+				glass = GlassEmpty.instance()
+			else:
+				glass = GlassHalf.instance()
 			glass.rect_position.x = x0 + (j * dx)
 			row.add_child(glass)
