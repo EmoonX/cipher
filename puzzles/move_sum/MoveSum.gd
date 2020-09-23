c943fcdd9f982b0f6b4a1c8e1a335033f681257a
Gustavo Chicato <gustavo.chicato@usp.br>
2020-09-23 04:58:40 -0300

    Calculate grid sums and check if game is won

--- puzzles/move_sum/MoveSum.gd
+++ puzzles/move_sum/MoveSum.gd
@@ -1,6 +1,9 @@
 extends "res://puzzles/Puzzle.gd"
 
-# Position of empty space
+# Matrix of grid numbers
+var grid = []
+
+# Empty space position
 var x0 = 2
 var y0 = 2
 
@@ -10,16 +13,52 @@ func _ready():
 	# Shuffle list of numbers
 	var numbers = [1, 2, 3, 4, 6, 7, 8, 9]
 	numbers.shuffle()
+	numbers.insert(4, 5)
+	
+	# Build initial matrix
+	for i in range(3):
+		grid.append([])
+		for j in range(3):
+			grid[i].append(numbers[3*i + j])
 	
 	# Set each piece with a different number
 	var i = 0
 	for piece in $Grid.get_children():
 		if piece is Label:
 			piece.text = str(numbers[i])
-			i += 1
 		else:
 			piece.get_child(0).text = "5"
-
-func _process(delta):
-	print(x0, " ", y0)
+		i += 1
 	
+	# Calculate initial sums
+	calculate_sums()
+
+func calculate_sums():
+	# Calculate sums on rows, columns and diagonals
+	var sums = {}
+	for node in $Sums.get_children():
+		var sum = 0
+		if node.name[0] == "H":
+			var i = int(node.name[1]) - 1
+			for j in range(3):
+				sum += grid[i][j]
+		elif node.name[0] == "V":
+			var j = int(node.name[1]) - 1
+			for i in range(3):
+				sum += grid[i][j]
+		elif node.name == "D1":
+			for i in range(3):
+				sum += grid[i][i]
+		elif node.name == "D2":
+			for i in range(3):
+				sum += grid[i][2-i]
+		
+		# Change text
+		node.text = str(sum)
+		
+		# Add sum to set
+		sums[sum] = true
+	
+	# If all sums are equal, you win!
+	if len(sums) == 1:
+		get_tree().quit()
--- puzzles/move_sum/Piece.gd
+++ puzzles/move_sum/Piece.gd
@@ -11,18 +11,29 @@ func _on_Piece_focus_entered():
 		# Middle piece can't be moved
 		return
 	
+	# Move piece on click, if adjacent to a blank position
 	var x0 = $"../..".x0
 	var y0 = $"../..".y0
 	var pos_list = [[x-1, y], [x+1, y], [x, y-1], [x, y+1]]
 	for pos in pos_list:
 		if pos[0] == x0 and pos[1] == y0:
-			print("OK")
+			# Reposition piece
 			rect_position.x = rect_size.x * (x0 - 1)
 			rect_position.y = rect_size.y * (y0 - 1)
+			
+			# Update grid matrix
+			$"../..".grid[y-1][x-1] = 5 if (x == 2 and y == 2) else 0
+			$"../..".grid[y0-1][x0-1] = int(text)
+			
+			# Swap values
 			$"../..".x0 = x
 			$"../..".y0 = y
 			x = x0
 			y = y0
+			
+			# Recalculate sums
+			$"../..".calculate_sums()
+			
 			break
 	
 	# Allow piece to be clicked again immediately
