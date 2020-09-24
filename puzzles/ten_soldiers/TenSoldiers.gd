e1a909dc2fab61d943d2d774f4b5ad3d5c0ff4dc
Gustavo Chicato <gustavo.chicato@usp.br>
2020-09-24 13:35:33 -0300

    Better drawing of soldier lines (and less bugs)

--- puzzles/ten_soldiers/TenSoldiers.gd
+++ puzzles/ten_soldiers/TenSoldiers.gd
@@ -4,45 +4,58 @@ extends "res://puzzles/Puzzle.gd"
 
 class CustomSorter:
 	static func sort_by_x(a: CanvasLayer, b: CanvasLayer):
+		# Sort pieces based on global x coordinate
 		var x1 = a.offset.x + a.get_node("Image").rect_position.x
 		var x2 = b.offset.x + b.get_node("Image").rect_position.x
 		return x1 < x2
 
 func _draw():
-	var pairs = []
+	# Get sorted list of soldiers
 	var soldiers = get_children()
 	soldiers.sort_custom(CustomSorter, "sort_by_x")
+	
+	# Iterate through all soldiers
 	for i in range(len(soldiers)):
 		var soldier = soldiers[i]
+		var used = []
 		var size = soldier.get_node("Image").rect_size
 		var pos1 = soldier.offset + soldier.get_node("Image").rect_position
 		pos1 += size / 2
+		
+		# Soldiers to the right of selected one
 		for j in range(i+1, len(soldiers)):
 			var other = soldiers[j]
-#			if [soldier, other] in pairs or [other, soldier] in pairs:
-#				continue
 			var pos2 = other.offset + other.get_node("Image").rect_position
 			pos2 += size / 2
 			
+			# Calculate initial angle
 			var dx = pos2.x - pos1.x
 			var dy = pos2.y - pos1.y
 			var alpha = atan2(dy, dx)
+			
+			# Build list of collinear soliders
 			var line = [soldier]
 			for k in range(j, len(soldiers)):
 				var another = soldiers[k]
-#				if [soldier, another] in pairs or [another, soldier] in pairs:
-#					continue
+				if another in used:
+					# Ignore soldiers from already formed lines
+					continue
 				pos2 = another.offset + another.get_node("Image").rect_position
 				pos2 += size / 2
 				
+				# Calculate angle
 				dx = pos2.x - pos1.x
 				dy = pos2.y - pos1.y
 				var beta = atan2(dy, dx)
-				if abs(beta - alpha) < 0.04:
+				
+				# If angle is below a certain threshold, then OK
+				if abs(beta - alpha) < 0.05:
 					line.append(another)
+					if len(line) == 4:
+						break
 			
-			#print(line)
 			if len(line) == 4:
+				# If line is formed, draw lines between soldiers in sequence
 				for l in range(3):
 					var pos3 = line[l].offset + \
 							line[l].get_node("Image").rect_position
@@ -51,7 +64,7 @@ func _draw():
 							line[l+1].get_node("Image").rect_position
 					pos4 += size / 2
 					draw_line(pos3, pos4, ColorN("crimson"), 5.0)
-					pairs.append([line[l], line[l+1]])
+					used.append(line[l+1])
 
 func _process(delta):
 	update()
