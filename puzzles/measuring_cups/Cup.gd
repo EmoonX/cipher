3f8abc1b977fcee6c7112c06e35393ac784bfe75
Gustavo Chicato <gustavo.chicato@usp.br>
2020-09-25 11:39:09 -0300

    Fluids can be poured between cups :)

--- puzzles/measuring_cups/Cup.gd
+++ puzzles/measuring_cups/Cup.gd
@@ -18,6 +18,10 @@ func _ready():
 	# Resize cup according to capacity
 	margin_top = -(capacity * DY)
 	
+	# Resize initial fluids
+	change_fluids()
+	
+func change_fluids():
 	# Resize water texture
 	var water_y = water * DY
 	$Water.texture.region.size.y = water_y
@@ -37,15 +41,37 @@ func _draw():
 		color.a = 0.5
 		draw_circle(rect_size / 2, rect_size.x / 2, color)
 
+func _pour_from(cup):
+	if oil + water < capacity:
+		# Pour oil
+		var d_oil = min(cup.oil, capacity - oil - water)
+		cup.oil -= d_oil
+		oil += d_oil
+		if oil + water < capacity:
+			# Pour water
+			var d_water = min(cup.water, capacity - oil - water)
+			cup.water -= d_water
+			water += d_water
+		
+		# Redraw fluids
+		cup.change_fluids()
+		change_fluids()
+
 func _on_Cup_gui_input(event):
 	if event is InputEventMouseButton and \
 			event.pressed and event.button_index == BUTTON_LEFT:
 		var action = false
 		for cup in $"..".get_children():
 			if cup != self and cup.selected:
+				# If one was already selected, pour from it
+				_pour_from(cup)
+				cup.selected = false
 				action = true
 				break;
+		
 		if not action:
 			# Select/unselect
 			selected = not selected
-			update()
+		
+		# Draw again
+		update()
